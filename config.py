90e98c7296d29265d19758c90f834deeeb1dfb5b
Dertox <deetox038@gmail.com>
2025-05-20 01:11:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+UNIVERSITIES = [
+    "Addis Ababa University", "Jimma University", "Bahir Dar University", "Hawassa University",
+    "Haramaya University", "University of Gondar", "Mekelle University", "Arba Minch University",
+    "Adama Science and Technology University", "Debre Berhan University", "Debre Markos University",
+    "Madda Walabu University", "Mizan Tepi University", "Aksum University", "Wollega University",
+    "Rift Valley University", "Addis Ababa Science and Technology University", "Assosa University",
+    "Wolkite University", "Unity University", "Wachemo University", "Jigjiga University",
+    "Ambo University", "Wolaita Sodo University", "Semera University", "Debre Tabor University",
+    "Bule Hora University", "Dire Dawa University", "Mekelle Institute of Technology",
+    "Woldia University", "Adigrat University", "Mettu University", "Dilla University",
+    "Graduate School of Telecommunication and Information Technology", "Wollo University"
+]
+
+REQUIRED_CHANNELS = ["@unimatch_ethio", "@unimatch_confession"]
+CONFESSION_CHANNEL_ID = "@unimatch_confession"
